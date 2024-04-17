25f60231de722e0b037746e4c2c51c26b8ade553
bwesterg <benton.westergaard@gmail.com>
2024-04-17 15:08:24 -0600

    adds navbar smoothscrolling and web3 contact form hook, testing functionality.

--- react-portfolio2/src/Components/Hero/Hero.jsx
+++ react-portfolio2/src/Components/Hero/Hero.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Hero.css';
 import BW_profile from "../../assets/BW_profile.jpg";
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 const Hero = () => {
   return (
-    <div className='hero'>
+    <div id="home" className='hero'>
       <div className="hero-image">
         <img src={BW_profile} />
       </div>
@@ -18,7 +19,9 @@ const Hero = () => {
         </p>
       <div className="hero-action">
         <div className="hero-connect">
+        <AnchorLink className='anchor-link' offset={50} href='#contact'>
           Connect with me
+        </AnchorLink>
         </div>
         <div className="hero-resume">My resume</div>
 
