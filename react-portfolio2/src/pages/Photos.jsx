0e10913aa3d729268934cd3bfafa33f273df500f
bwesterg <benton.westergaard@gmail.com>
2024-06-06 15:25:47 -0600

    adds and formats photos using temp mock data file

--- react-portfolio2/src/pages/Photos.jsx
+++ react-portfolio2/src/pages/Photos.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../Components/Navbar/Navbar';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import Contact from '../Components/Contact/Contact';
@@ -6,12 +6,17 @@ import Footer from '../Components/Footer/Footer';
 import theme_pattern from '../assets/theme_pattern.svg';
 
 import './Photos.css';
+import images from './api-mock.json';
 
 const Photos = () => {
+  const [imageList, setImageList] = useState(images.resources);
   return (
     <>
       <Navbar />
       <div className="photos-page-container">
+        <div className="image-grid">
+          {imageList.map((image, index)=><img key={index} src={image.url} alt={image.public_id}/>)}
+        </div>
         <Contact />
       </div>
       <Footer />
