661dc774b6cd697ddc7a7c5d7a9b03ef619182a9
bwesterg <benton.westergaard@gmail.com>
2024-06-09 15:57:53 -0600

    adds photo gallery with lightbox and styles

--- react-portfolio2/src/pages/api.js
+++ react-portfolio2/src/pages/api.js
@@ -1,9 +1,16 @@
 const API_URL = import.meta.env.VITE_API_URL;
 
 
-export const getImages = async () => {
-  const response = await fetch(`${API_URL}/photos`);
-  // const response = await fetch(`http://localhost:7000/photos`);
+export const getImages = async (nextCursor) => {
+  const params = new URLSearchParams();
+
+  if(nextCursor){
+    params.append('next_cursor', nextCursor);
+  }
+
+  const response = await fetch(`${API_URL}/photos?${params}`);
+  //The first time request runs, params is not included 
+  //because the cloudinary api returns the first 10 results.
   const responseJson = await response.json();
   return responseJson;
 }
